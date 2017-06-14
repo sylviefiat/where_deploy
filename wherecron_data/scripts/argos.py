ddacb731497602032ed369802f8e3fc07b7dd1ce
Sylvie Fiat <sylvie.fiat@ird.fr>
2017-06-14 16:19:42 +1100

    production ready

--- wherecron_data/scripts/argos.py
+++ wherecron_data/scripts/argos.py
@@ -3,9 +3,11 @@ import re
 import csv
 import math
 import datetime
+import dateutil.parser
 import smtplib
 import mimetypes
-import MySQLdb
+#import MySQLdb
+
 from email.mime.multipart import MIMEMultipart
 from email import encoders
 from email.message import Message
@@ -17,6 +19,8 @@ from email.mime.multipart import MIMEMultipart
 from subprocess import Popen, PIPE
 import xml.dom.minidom
 import xml.etree.ElementTree as ET
+import pymysql
+pymysql.install_as_MySQLdb()
 
 #http://ws-argos.cls.fr/argosDws/services/DixService?wsdl
 ARGOS_HOST = "ws-argos.cls.fr"
@@ -26,41 +30,15 @@ def argosRequest(request):
     conn.request("POST", "/argosDws/services/DixService", request)
     response = conn.getresponse()
     #print response.status, response.reason, response.msg
-    data = response.read()
-    print(data)
+    data = response.read().decode('utf-8')    
     conn.close()
     return data
-
-def cleanupXml(data):
-    body = re.search("<return>(.*)</return>", data, flags=re.S)
-    if (body):
-        body = body.group(1)
-        body = re.sub("&lt;", "<", body)
-        body = re.sub("&gt;", ">", body)
-        body = re.sub("&amp;", "&", body)
-        body = re.sub("&lt;", "<", body)
-        ugly_xml = xml.dom.minidom.parseString(body)
-        ugly_xml = ugly_xml.toprettyxml(indent='  ')
-        text_re = re.compile('>\n\s+([^<>\s].*?)\n\s+</', re.DOTALL)    
-        pretty_xml = text_re.sub('>\g<1></', ugly_xml)
-        return pretty_xml
 
 def cleanupCsv(data):
     body = re.search("<return>(.*)</return>", data, flags=re.S)
     if (body):
         body = body.group(1)
         return body
-
-
-def getPlatforms(username, password, programNumber):
-    argosXml = getXml(username, password, programNumber, type="program", nbPassByPtt=1)
-    root = ET.fromstring(argosXml)
-    platformIds = []
-    for platform in root.findall(".//platform"):
-        #ET.dump(platform)
-        platformIds.append(int(platform.find("platformId").text))
-    return platformIds
-
 
 
 def getCsv(username, password, id, type="platform", nbPassByPtt=10, nbDaysFromNow=10, mostRecentPassages="true", displaySensor="false"):
@@ -88,122 +66,23 @@ def getCsv(username, password, id, type="platform", nbPassByPtt=10, nbDaysFromNo
         '</soap:Body>\n'
         '</soap:Envelope>'
         ) % (username, password, type, nbPassByPtt, nbDaysFromNow, mostRecentPassages, displaySensor)
-
-
-    #print request
     data = argosRequest(request)
     data = cleanupCsv(data)
-    print data
     return data
 
-
-def getKml(username, password, id, type="platform", nbPassByPtt=10, nbDaysFromNow=10, mostRecentPassages="true"):
-    if (type == "program"):
-        type = "<typ:programNumber>" + str(id) + "</typ:programNumber>"
-    else:
-        type = "<typ:platformId>" + str(id) + "</typ:platformId>"
-
-    request = (
-        '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:typ="http://service.dataxmldistribution.argos.cls.fr/types">\n'
-        '<soap:Header/>\n'
-        '<soap:Body>\n'
-        '<typ:kmlRequest>\n'
-        '<typ:username>%s</typ:username>\n'
-        '<typ:password>%s</typ:password>\n'
-        '%s\n'
-        '<typ:nbPassByPtt>%d</typ:nbPassByPtt>\n'
-        '<typ:nbDaysFromNow>%d</typ:nbDaysFromNow>\n'
-        '<typ:mostRecentPassages>%s</typ:mostRecentPassages>\n'
-        '<typ:displayDescription>true</typ:displayDescription>\n'
-        '</typ:kmlRequest>\n'
-        '</soap:Body>\n'
-        '</soap:Envelope>'
-        ) % (username, password, type, nbPassByPtt, nbDaysFromNow, mostRecentPassages)
-
-
-    #print request
-    data = argosRequest(request)
-    data = cleanupXml(data)
-    return data
-
-
-def getXml(username, password, id, type="platform", nbPassByPtt=10, nbDaysFromNow=10, mostRecentPassages="true", displaySensor="false"):
-    if (type == "program"):
-        type = "<typ:programNumber>" + str(id) + "</typ:programNumber>"
-    else:
-        type = "<typ:platformId>" + str(id) + "</typ:platformId>"
-
-    request = (
-        '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:typ="http://service.dataxmldistribution.argos.cls.fr/types">\n'
-        '<soap:Header/>\n'
-        '<soap:Body>\n'
-        '<typ:xmlRequest>\n'
-        '<typ:username>%s</typ:username>\n'
-        '<typ:password>%s</typ:password>\n'
-        '%s\n'
-        '<typ:nbPassByPtt>%d</typ:nbPassByPtt>\n'
-        '<typ:nbDaysFromNow>%d</typ:nbDaysFromNow>\n'
-        '<typ:mostRecentPassages>%s</typ:mostRecentPassages>\n'
-        '<typ:displayLocation>true</typ:displayLocation>\n'
-        '<typ:displayRawData>false</typ:displayRawData>\n'
-        '<typ:displaySensor>%s</typ:displaySensor>\n'
-        '</typ:xmlRequest>\n'
-        '</soap:Body>\n'
-        '</soap:Envelope>'
-        ) % (username, password, type, nbPassByPtt, nbDaysFromNow, mostRecentPassages, displaySensor)
-
-
-    #print request
-    data = argosRequest(request)
-    data = cleanupXml(data)
-    return data
-
-def getXsd():
-    request = (
-        '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:typ="http://service.dataxmldistribution.argos.cls.fr/types">\n'
-        '<soap:Header/>\n'
-        '<soap:Body>\n'
-        '<typ:xsdRequest/>\n'
-        '</soap:Body>\n'
-        '</soap:Envelope>'
-        ) % ()
-
-
-    #print request
-    data = argosRequest(request)
-    data = cleanupXml(data)
-    return data
-
-
-def getLocations(argos_xml):
-    root = ET.fromstring(argos_xml)
-    locations = []
-    for location in root.findall(".//location"):
-        #ET.dump(location)
-        location_date = location.find("locationDate").text
-        latitude = float(location.find("latitude").text)
-        longitude = float(location.find("longitude").text)
-        current_location = [location_date, latitude, longitude]
-        locations.append(current_location)
-
-    if (len(locations) == 0):
-        return None
-    else:
-        return locations
-
-def get_current_location(username, password, platform_id):
-    argos_xml = getXml(username, password, platform_id)
-    locations = getLocations(argos_xml)
-    current_loc = sorted(locations).pop()
-    return current_loc
+def saveCsv(data):
+    filename= "ArgosData_WHERE_"+datetime.datetime.today().strftime('%Y-%m-%d')+"_FULL.csv"
+    with open("./export/"+filename, 'w') as f:
+        f.write(data)
+    return "./export/"+filename
 
 def calcul_speed(latitude1,longitude1, date1, latitude2, longitude2, date2):
     lon1=float(longitude1.replace(',','.'))
     lon2=float(longitude2.replace(',','.'))
     lat1=float(latitude1.replace(',','.'))
     lat2=float(latitude2.replace(',','.')) 
-    d1=datetime.datetime.strptime(date1, '%Y/%m/%d %H:%M:%S')
-    d2=datetime.datetime.strptime(date2, '%Y/%m/%d %H:%M:%S')
+    d1=dateutil.parser.parse(date1)
+    d2=dateutil.parser.parse(date2)
     distance = math.acos(math.sin(math.radians(lon1))*math.sin(math.radians(lon2))+math.cos(math.radians(lon1))*math.cos(math.radians(lon2))*math.cos(math.radians(lat1-lat2)))*6371
     elapsedTime = d2 - d1 
     temps = elapsedTime.days * 24 + elapsedTime.seconds / 3600.0
@@ -225,44 +104,39 @@ def checkTableExists(dbcon, tablename):
     return False
 
 def createTable(dbcon):
-    cursor.execute("""CREATE TABLE where(id INT PRIMARY KEY,plateforme INT,programme INT,\
-    lat_dms VARCHAR(255),lat_dd VARCHAR(255),lon_dms VARCHAR(255),lon_dd VARCHAR(255),cap VARCHAR(255),\
-    vitesse VARCHAR(255),qualite_loc VARCHAR(255), date_loc DATE,altitude VARCHAR(255),passage VARCHAR(255),\
-    sat VARCHAR(255),frequence VARCHAR(255),date_msg DATE, comp VARCHAR(255),msg INT,120DB VARCHAR(255),\
-    best_level VARCHAR(255),delta_freq VARCHAR(255),long1 VARCHAR(255),lat1 VARCHAR(255),long2 VARCHAR(255),lat2 VARCHAR(255),loc_idx INT,\
-    nopc INT,rayon_err INT,demi_gd_axe INT,demi_petit_axe INT,angle_ellipse INT,GDOP INT,nom_format VARCHAR(255),sensor01 INT,sensor02 INT,sensor03 INT,\
-    sensor04 INT,sensor05 INT,sensor06 INT,sensor07 INT,sensor08 INT,sensor09 INT,sensor10 INT,sensor11 INT,\
-    sensor12 INT,sensor13 INT,sensor14 INT,sensor15 INT,sensor16 INT,sensor17 INT,sensor18 INT,sensor19 INT,sensor20 INT,sensor21 INT,sensor22 INT,\
-    sensor23 INT,sensor24 INT,sensor25 INT,sensor26 INT,sensor27 INT,sensor28 INT,sensor29 INT,sensor30 INT,sensor31 INT)""")
+    dbcur = dbcon.cursor()
+    dbcur.execute("""CREATE TABLE where_whales(programNumber INT,platformId INT,platformType VARCHAR(255),
+    platformModel VARCHAR(255),platformName VARCHAR(255),satellite VARCHAR(255),bestMsgDate TIMESTAMP,duration INT,
+    nbMessage INT,message120 INT, bestLevel INT,frequency FLOAT,locationDate TIMESTAMP,latitude FLOAT,
+    longitude FLOAT,altitude FLOAT,locationClass VARCHAR(255), gpsSpeed VARCHAR(255),gpsHeading VARCHAR(255),PRIMARY KEY (platformId, locationDate))""")
+    dbcur.close()
 	
 
 def insert_csv(host,user,passwd,db,csvfile):
-    mydb = MySQLdb.connect(host=host,
+    mydb = pymysql.connect(host=host,
         user=user,
         passwd=passwd,
         db=db)
-        cursor = mydb.cursor()
-    if checkTableExists(mysql, "where") is False
-        createTable(mysql)
-    csv_data = csv.reader(file(csvfile))
-    for row in csv_data:
-        cursor.execute("INSERT INTO where(id,plateforme,programme,lat_dms,lat_dd,lon_dms,lon_dd,cap,vitesse,qualite_loc,date_loc,altitude,passage,sat,frequence,date_msg,\
-        comp,msg,120DB,best_level,delta_freq,long1,lat1,long2,lat2,loc_idx,nopc,rayon_err,demi_gd_axe,demi_petit_axe,angle_ellipse,GDOP,nom_format,sensor01,sensor02,sensor03,\
-        sensor04,sensor05,sensor06,sensor07,sensor08,sensor09,sensor10,sensor11,sensor12,sensor13,sensor14,sensor15,sensor16,sensor17,sensor18,sensor19,sensor20,sensor21,sensor22,\
-        sensor23,sensor24,sensor25,sensor26,sensor27,sensor28,sensor29,sensor30,sensor31) \
-        VALUES(%d, %d, %d, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %d, %s, %s, %s, %s, %s, %s, %s, %d, %d, %d, %d, %d, %d, %d, %s, %d, %d, %d,\
-        %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d)", row)
+    cursor = mydb.cursor()
+    if not checkTableExists(mydb,"where_whales"):
+        createTable(mydb)
+    reader = csv.reader(open(csvfile,"r"),delimiter=';')
+    next(reader, None) 
+    for row in reader:
+        cursor.execute("""INSERT IGNORE INTO where_whales(programNumber,platformId,platformType,platformModel,platformName,
+        satellite,bestMsgDate,duration,nbMessage,message120,bestLevel,frequency,locationDate,latitude,longitude,altitude,
+        locationClass,gpsSpeed,gpsHeading) VALUES('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}') 
+        """.format(int(row[0]),int(row[1]),row[2],row[3],row[4],row[5],dateutil.parser.parse(row[6]),int(row[7]),int(row[8]),int(row[9]),int(row[10]),float(row[11]),dateutil.parser.parse(row[12]),float(row[13]),float(row[14]),float(row[15]),row[16],row[17],row[18]))
  
     #close the connection to the database.
     mydb.commit()
     cursor.close()
-    print "Done" 
 
 def convertCSV_for_DTSI(file):    
     with open(file, 'r', encoding="utf8") as f:        
-        fieldnames = ['N° ID','Date de loc.','Latitude (degré décimal)','Longitude  (degré décimal)','Chronologie'] 
-        filename= "argos_where_"+datetime.datetime.today().strftime('%Y-%m-%d')+".csv"
-        writer = csv.DictWriter(open("./exemples/"+filename, 'w'), fieldnames=fieldnames,delimiter=';')
+        fieldnames = ['platformId','locationDate','latitude','longitude','chronologie'] 
+        filename= "ArgosData_WHERE__"+datetime.datetime.today().strftime('%Y-%m-%d')+"_AFFMAR_DTSI.csv"
+        writer = csv.DictWriter(open("./export/"+filename, 'w'), fieldnames=fieldnames,delimiter=';')
         writer.writeheader()
 
         reader = csv.DictReader(f,delimiter=';')
@@ -286,7 +160,7 @@ def convertCSV_for_DTSI(file):
                     longitude1=row[fieldnames[2]]
                     date1=row[fieldnames[1]]
                     n = n+1
-    return "./exemples/"+filename
+    return "./export/"+filename
 
 def sendcsv_mail_with_sendmail(expediteur,destinataire,fileToSend):
     html = MIMEText("<html><head><title>New argos data</title></head><body>Please find attached new argos data for WHERE project</body>", "html")
@@ -301,11 +175,18 @@ def sendcsv_mail_with_sendmail(expediteur,destinataire,fileToSend):
 
 
 if __name__ == '__main__':
-    getCsv('GARRIGUE','BOSSE_2016',6145,'program',10, 20, "true", "false")
-    argos_csv='./exemples/ArgosData_2016_09_23-25.csv'
-    insert_csv('wheredb','root','docker','where',csvfile)
-    file = convertCSV_for_DTSI(argos_csv)
-    #sendcsv_mail_with_google("devtest.ird@gmail.com","sylvie.fiat@ird.fr",file,"devtest.ird@gmail.com","DEV2016test")
-    sendcsv_mail_with_sendmail("sylvie.fiat@ird.fr","sylvie.fiat@ird.fr",file)
+    # if running on monday query 4 days, orherwise (thursday) query 3 days
+    dayofweek=datetime.datetime.today().weekday()
+    dayfromnow=4 if (dayofweek==1) else 3
+    # query argos web service
+    argos_csv=getCsv('GARRIGUE','BOSSE_2016',6145,'program',dayfromnow, 20, "true", "false")
+    #save csv into file
+    argos_file=saveCsv(argos_csv)
+    # save data into database
+    insert_csv('wheredb','root','docker','baleines',argos_file)
+    # convert for DTSI
+    dtsi_file = convertCSV_for_DTSI(argos_file)
+    # send mail
+    sendcsv_mail_with_sendmail("sylvie.fiat@ird.fr","sylvie.fiat@ird.fr",dtsi_file)
 
     
---
+++ wherecron_data/scripts/run_argos.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+container_exists=`docker images | grep python_where`
+echo $container_exists
+[[ -z "$container_exists" ]] && docker build -t python_where /scripts
+docker run --rm --name argos-where --net wheredeploy_default  --link wheredeploy_wheredb_1:wheredb -v /opt/docker/where_deploy/wherecron_data/scripts/export:/export -v /opt/docker/where_deploy/wherecron_data/scripts/argos.py:/argos.py -w / python_where:latest
+
