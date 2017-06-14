de32d54a229d9d8671b422bab25ecd5fe1bac791
Sylvie Fiat <sylvie.fiat@ird.fr>
2017-06-14 16:37:33 +1100

    config ports for sendmail

--- wherecron_data/scripts/run_argos.sh
+++ wherecron_data/scripts/run_argos.sh
@@ -2,5 +2,5 @@
 container_exists=`docker images | grep python_where`
 echo $container_exists
 [[ -z "$container_exists" ]] && docker build -t python_where /scripts
-docker run --rm --name argos-where --net wheredeploy_default  --link wheredeploy_wheredb_1:wheredb -v /opt/docker/where_deploy/wherecron_data/scripts/export:/export -w / python_where:latest
+docker run --rm --name argos-where --net wheredeploy_default  --link wheredeploy_wheredb_1:wheredb -v /opt/docker/where_deploy/wherecron_data/scripts/export:/export -w / -p 25:25 python_where:latest
 
